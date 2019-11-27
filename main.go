96a58ae02bc68bec586d69b64e8eb5fb764c1bec
Arda Çetinkaya <thearda@hotmail.com>
2019-11-27 10:41:56 +0000

    http methods

--- main.go
+++ main.go
@@ -4,32 +4,52 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
+
+	"./config"
 )
 
+//Log mode
 type Log struct {
 	Text    string    `json:"text"`
 	LogType string    `json:"type"`
 	Date    time.Time `json:"date"`
 }
 
+//Logs variable
 type Logs []Log
 
 func logs(w http.ResponseWriter, req *http.Request) {
-	logs := Logs{
-		Log{Text: "System is running", LogType: "Info", Date: time.Now()},
-		Log{Text: "Error", LogType: "Error", Date: time.Now()},
+	switch req.Method {
+	case "GET":
+		logs := Logs{
+			Log{Text: "System is running", Date: time.Now()},
+			Log{Text: "Error", Date: time.Now()},
+		}
+
+		if err := json.NewEncoder(w).Encode(logs); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	case "POST":
+		var l Log
+		err := json.NewDecoder(req.Body).Decode(&l)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		//TODO: Save
+	default:
+		http.Error(w, "Invalid method", http.StatusBadRequest)
 	}
 
-	if err := json.NewEncoder(w).Encode(logs); err != nil {
-		panic(err)
-	}
 }
 
 func main() {
 
+	configFile := config.LoadConfiguration("config.json")
+
 	http.HandleFunc("/logs", logs)
 
-	if err := http.ListenAndServe(":8090", nil); err != nil {
+	if err := http.ListenAndServe(configFile.Port, nil); err != nil {
 		panic(err)
 	}
 }
